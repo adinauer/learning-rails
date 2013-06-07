a015c024dbaf99ff7a2685892ebf05f40bfc4429
Alexander Dinauer <alexander@dinauer.at>
2013-06-07 11:37:43 +0200

    add comment that shows how to redirect to a different page

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,5 +3,8 @@ class ClientsController < ApplicationController
     puts "blablub"
     @testBla = "lorem ipsum dolor sit amet"
     @para = params
+
+    # redirect_to root_url
+    # redirect_to statuses_url
   end
 end
