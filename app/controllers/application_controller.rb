bd7919d170cbd990d9029fef60b3c36aea146782
Alexander Dinauer <alexander@dinauer.at>
2013-06-11 12:50:58 +0200

    dynamically fetch categories from server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
+require 'java'
+
+java_import 'at.dinauer.fhbay.util.DataFetcher'
+
+class RailsDataStore
+  attr_reader :categories
+
+  def store_categories(categories)
+    puts "categories: #{categories.size}"
+    categories.each do |category|
+      puts category.name
+    end
+
+    @categories = categories
+  end
+end
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :find_categories
+
+  def find_categories
+    fetcher = DataFetcher.new
+    dataStore = RailsDataStore.new
+    fetcher.fetchCategories(dataStore)
+
+    @categories = dataStore.categories
+  end
 end
